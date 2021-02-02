fef1ed4befaf28ac9a80e9fd8c0cf40981447079
jubick1337 <Lytdybr99>
2021-02-02 17:43:51 +0500

    init

---
+++ src/test/kotlin/com/nat/nat/NatApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nat.nat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NatApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
